2196f0b063b4a154eca3ae1612c7e7b890a278ba
Luke Westby <lwestby@alumni.nd.edu>
2016-10-27 11:02:08 -0500

    add note about syncing to deploy script output

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,6 @@ git add -A
 git commit -m "build site"
 git push origin master
 git subtree push --prefix=dist git@github.com:nrgmeeting/idsite-src build
+
+
+echo "\n\nDon't forget to sync the git repo in the Stormpath ID Site Admin settings!"
